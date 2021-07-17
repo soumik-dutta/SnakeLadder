b5289cc7f95d0ec82ee016c972fea8be68a1f4ea
Soumik <soumik.dutta@sixt.com>
2021-07-17 21:04:46 +0530

    Adding test cases

--- src/main/java/service/DiceService.java
+++ src/main/java/service/DiceService.java
@@ -2,8 +2,44 @@ package service;
 
 import java.util.Random;
 
+/**
+ * Creating a singleton object for dice as this cannot change during the game
+ * it will be initialized during the game startup
+ */
 public class DiceService {
-    public static int roll() {
-        return new Random().nextInt(6) + 1;
+    private static DiceService diceService = null;
+    private static final String NORMAL = "NORMAL";
+    private static final String CROOCKED = "CROOCKED";
+    private static String TYPE = NORMAL;
+    private static Random rand = new Random();
+
+    public int roll() {
+        int randNumber = rand.nextInt(6) + 1;
+        if (TYPE.equals(CROOCKED) && randNumber % 2 !=0) {
+            randNumber = roll();
+        }
+        return randNumber;
     }
+
+    private DiceService(int number) {
+        if (number % 2 == 0) {
+            TYPE = CROOCKED;
+            System.out.println("SETTING DICE AS " + TYPE);
+        }
+    }
+
+    public static DiceService getDiceService() {
+        if (diceService == null) {
+            // setting DICE strategy to NORMAL by default
+            diceService = new DiceService(1);
+        }
+        return diceService;
+    }
+
+    public static void setDiceService(Integer number) {
+        if (diceService == null && number != null) {
+            diceService = new DiceService(number);
+        }
+    }
+
 }
