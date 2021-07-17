a68f24e16509b2010ebd955286ce0902d81c0e2c
soumik dutta <soumik652@hotmail.com>
2021-07-17 18:27:34 +0530

    Merge pull request #1
    * Initial commit : barebone files
    
    * Initial commit : basic snake and ladder game

---
+++ src/main/java/domain/Board.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Board {
+    private List<Player> players;
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+}
---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.UUID;
+
+public class Player {
+    private String name;
+    private String id;
+
+    public Player(String name) {
+        this.name = name;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
